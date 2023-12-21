6b9f497ebb0968fa64b524ae2b1c3b230dc3a0e7
Deroh9 <154458561+Deroh9@users.noreply.github.com>
2023-12-22 00:09:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254791006529', 'Deroh', true],
+  ['447451241865', 'Puffy', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '༒ᴘʀ†ɴᴄᴇss ᴀʏʀᴀ༒'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'ʟɪʟ ᴘᴜғғʏ𓅂' 
+global.author = '✘ʏᴏᴜʀ ᴅᴀᴅ✘' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
